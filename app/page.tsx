727421c144b44056b08d6b6dbef400e264013ae5
Cikgu Nic <g-56148390@moe-dl.edu.my>
2026-05-26 10:21:27 +0800

    UPADATE LOGIN

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,19 @@ async function handleLogin(e: React.FormEvent) {
 
     try {
       const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbzeGCohq7mGAcQ7igJryYX7Nba3SkZPLDluj44K-Cps1CwWuOEpNdxAGkL4RwBc1nfjLQ/exec",
-        {
-          method: "POST",
-          body: JSON.stringify({ action: "LOGIN", id: idMurid, password: kataLaluan }),
-        }
-      );
+  "https://script.google.com/macros/s/AKfycbzeGCohq7mGAcQ7igJryYX7Nba3SkZPLDluj44K-Cps1CwWuOEpNdxAGkL4RwBc1nfjLQ/exec",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      action:"LOGIN",
+      id:idMurid,
+      password:kataLaluan
+    })
+  }
+);
 
       console.log("2. Google dah respon! Status Code:", response.status);
 
