e49e5a8d96ab95c817ce965c185d6ccc1abe724e
Cikgu Nic <g-56148390@moe-dl.edu.my>
2026-06-01 23:01:13 +0800

    api

--- app/api/semak-ai/route.ts
+++ app/api/semak-ai/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
     ];
 
     const model = genAI.getGenerativeModel({ 
-        model: "gemini-2.5-flash", 
+        model: "gemini-flash-latest", 
         safetySettings: safetySettings 
     });
 
